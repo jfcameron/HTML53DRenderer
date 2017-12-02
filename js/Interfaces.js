d3e3b6c2359db0867ecb5201fa24aa5441ce40a9
jfcameron <jf.cameron@gmail.com>
2017-12-02 07:09:47 +0000

    eod

--- js/Interfaces.js
+++ js/Interfaces.js
@@ -1,30 +1,22 @@
 // © 2017 Joseph Cameron - All Rights Reserved
 // Project: CSS3DRenderer
-// Created on 2017-12-01.
+// Created on 2017-12-02.
 "use strict";
 
 define(
 [
-    
+    "./Exceptions"
 ], 
 function() 
 {
-    var Vector3 = function()
+    var Interfaces = function()
     {
         // Public interface
-        this.x = 0;
-        this.y = 0;
-        this.z = 0;
         
         // Constructors
         if (arguments.length == 0)
         {
-        }
-        else if (arguments.length == 3 && !isNaN(arguments[0]) && !isNaN(arguments[1] && !isNaN(arguments[2])))
-        {
-            this.x = arguments[0];
-            this.y = arguments[1];
-            this.z = arguments[2];
+            throw "Interfaces is a stub!";
         }
         else
         {
@@ -32,7 +24,7 @@ function()
         }
     };
 
-    Vector3.prototype.Tag = "Vector3";
+    Interfaces.prototype.Tag = "Interfaces";
 
-    return Vector3;
+    return Interfaces;
 });
