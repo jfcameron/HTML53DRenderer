b5d56eda00d506688010d3f9869bcc1e118daf9a
Joseph Cameron <jf.cameron@gmail.com>
2017-12-18 20:38:39 -0500

    wip

--- Engine/test/sum.js
+++ Engine/test/sum.js
@@ -1,6 +1,4 @@
-function sum(a, b) 
-{
-  return a + b;
-}
-
-export default sum;
+function sum(a, b) {
+    return a + b;
+  }
+  module.exports = sum;
--- Engine/test/sum.test.js
+++ Engine/test/sum.test.js
@@ -1,6 +1,5 @@
-import sum from './sum'
+const sum = require('./sum');
 
-test('adds 1 + 2 to equal 3', () => 
-{
-    expect(sum(1, 2)).toBe(3);
+test('adds 1 + 2 to equal 3', () => {
+  expect(sum(1, 2)).toBe(3);
 });
