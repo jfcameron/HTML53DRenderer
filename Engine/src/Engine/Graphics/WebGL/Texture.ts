d30d91cb81ee3f361caebda8cf12647f52b066c2
Joseph Cameron <jf.cameron@gmail.com>
2018-01-10 22:14:03 -0500

    wip

--- Engine/src/Engine/Graphics/WebGL/Texture.ts
+++ Engine/src/Engine/Graphics/WebGL/Texture.ts
@@ -30,12 +30,13 @@ class Texture
         {
             gl.bindTexture(gl.TEXTURE_2D, this.m_TextureHandle);
 
-            gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, //Set up the texture
-            gl.UNSIGNED_BYTE, image);
-        
+            gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
+
             gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
             gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR_MIPMAP_NEAREST);
+            
             gl.generateMipmap(gl.TEXTURE_2D);
+            
             gl.bindTexture(gl.TEXTURE_2D, null);    
         }
     }
